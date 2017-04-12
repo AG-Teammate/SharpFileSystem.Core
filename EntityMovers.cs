7c89fe327f2301873e2e74c840912bb6e1c5793a
Andrew Grishin <AG-Teammate@users.noreply.github.com>
2017-04-12 22:28:39 +0300

    initial commit

---
+++ EntityMovers.cs
@@ -0,0 +1,14 @@
+using SharpFileSystem.Collections;
+
+namespace SharpFileSystem
+{
+    public static class EntityMovers
+    {
+        public static TypeCombinationDictionary<IEntityMover> Registration { get; private set; }
+
+        static EntityMovers()
+        {
+            Registration = new TypeCombinationDictionary<IEntityMover>();
+        }
+    }
+}
---
+++ IO/ClosingStreamBase.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace SharpFileSystem.IO
+{
+    public abstract class ClosingStreamBase : Stream
+    {
+        //AG: they forgot Stream.Close in .NET Core so adding it
+        public virtual void Close()
+        {
+            this.Dispose();
+        }
+    }
+}
